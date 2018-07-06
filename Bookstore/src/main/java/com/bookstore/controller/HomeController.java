2cf1b947a8bd5b23f6dd78048123e2abc43eef68
Arkadiusz <arkadiusz.maliszewski@gmail.com>
2018-07-06 12:32:02 +0200

    security classes modification and home controller modification

--- Bookstore/src/main/java/com/bookstore/controller/HomeController.java
+++ Bookstore/src/main/java/com/bookstore/controller/HomeController.java
@@ -1,11 +1,30 @@
 package com.bookstore.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bookstore.domain.User;
+import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.service.UserService;
+import com.bookstore.service.impl.UserSecurityService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private UserSecurityService userSecurityService;
 
 	@RequestMapping("/")
 	public String index() {
@@ -18,9 +37,35 @@ public class HomeController {
 	}*/
 	
 	@RequestMapping("/newUser")
-	public String newUser(Model model) {
-		model.addAttribute("classActiveNewAccount", true);
-		return "myAccount";
+	public String newUser(
+			Locale locale,
+			@RequestParam("token") String token,
+			Model model) {
+		PasswordResetToken passToken = userService.getPasswordResetToken(token);
+		
+		if(passToken == null) {
+			String  message = "Invalid Token.";
+			model.addAttribute("message", message);
+			return "redirect:/badRequest";
+		}
+		
+		//if user can be found by the token(in database) and assigning name
+		User user = passToken.getUser();
+		String username = user.getUsername();
+		
+		//1.getting user details from user security service
+		UserDetails userDetails = userSecurityService.loadUserByUsername(username);
+		
+		//2.defining authentication environment
+		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+		
+		//3.retrieving security context and setting authentication to current user
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		//1,2,3 -> makes sure that the logged in session is assigned for that one user
+		
+		model.addAttribute("classActiveEdit", true);
+		return "myProfile";
 	}
 	
 	@RequestMapping("/login")
